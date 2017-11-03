0d6eace7c59648af5fd77d8f0b7748e2f66f9959
crapthings <crapthings@gmail.com>
2017-11-03 18:10:10 +0800

    minor update

--- client/components/layout/index.js
+++ client/components/layout/index.js
@@ -1,18 +1,29 @@
+import Grid from 'material-ui/Grid'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 import Mocks from './mocks'
 
-Layout = ({ children }) => <DragDropContext onDragEnd={() => {
-  console.log(123)
-}}>
-  <Droppable droppableId="droppable">
-    {(provided, snapshot) => {
-      return <div
-        ref={provided.innerRef}
-      >
-        {children()}
-        <Mocks />
-      </div>
-    }}
-  </Droppable>
-</DragDropContext>
+class Layout extends Component {
+  render () {
+    return <DragDropContext onDragEnd={result => {
+      console.log(result)
+    }}>
+      <Droppable droppableId='droppable'>
+        {(provided, snapshot) => {
+          return <div
+            ref={provided.innerRef}
+          >
+            <Grid container className={'root'}>
+              <Grid item xs={10}>
+                {children()}
+              </Grid>
+              <Grid item xs={2}>
+                <Mocks />
+              </Grid>
+            </Grid>
+          </div>
+        }}
+      </Droppable>
+    </DragDropContext>
+  }
+}
--- client/components/layout/mocks.js
+++ client/components/layout/mocks.js
@@ -1,11 +1,17 @@
 import { Draggable } from 'react-beautiful-dnd'
-
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 export default () => <div>
-  <ul>
-    <li>
-      <Draggable key={1} draggableId={1}>
-        {(provided, snapshot) => <span ref={provided.innerRef} {...provided.dragHandleProps}>div</span>}
-      </Draggable>
-    </li>
-  </ul>
+  <Draggable key={1} draggableId={1}>
+    {(provided, snapshot) => <div ref={provided.innerRef} {...provided.dragHandleProps}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            Title
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>}
+  </Draggable>
 </div>
